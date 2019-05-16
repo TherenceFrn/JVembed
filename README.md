874abec787af531cdd1f80a61147ccc1366ea0d4
Therence FERRON <45689459+TherenceFrn@users.noreply.github.com>
2019-05-16 14:09:47 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,77 @@
+# JVembed
+
+JVembed est un script qui permet d'intégrer dans la page tous les liens vers des images ( png, jpg, jpeg, gif) et vers des webm.
+
+
+## Installation
+
+Il vous faut une extension qui permet d'installer des script
+exemple :
+
+- Chrome :
+-[TamperMonkey]([https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=fr](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=fr))
+-[Chrome User Script]([https://chrome.google.com/webstore/detail/chrome-user-script-handle/dofpeochehjcckohddplnekbcndhcoid?hl=fr](https://chrome.google.com/webstore/detail/chrome-user-script-handle/dofpeochehjcckohddplnekbcndhcoid?hl=fr))
+
+- Firefox :
+-[TamperMonkey](https://addons.mozilla.org/fr/firefox/addon/tampermonkey/)
+-[Grease Monkeydons](https://addons.mozilla.org/fr/firefox/addon/greasemonkey/)
+
+- Opera
+-[TamperMonkey](https://addons.opera.com/fr/extensions/details/tampermonkey-beta/?display=en)
+
+- Safari
+-[TamperMonkey](https://tampermonkey.net/?browser=safari)
+
+- Android ( seulement sur firefox )
+-[Firefox]( https://play.google.com/store/apps/details?id=org.mozilla.firefox&hl=fr )
+et et le plugin 'USI' : https://addons.mozilla.org/fr/android/addon/userunified-script-injector/?src=search
+(Si vous utilisez un autre navigateur cherchez un plugin de type Tampermonkey/greasemonkey sur Google et avec un peu de chance vous trouverez)
+
+ensuite il vous suffit de cliquer [ici](https://google.fr)
+le script est sur [https://openuserjs.org/](https://openuserjs.org/)
+
+## Modifier le script
+
+### Imgur/4Chan
+
+Si vous ne souhaitez pas que toutes les images et webm soient intégrés,
+et que seulement le contenu provenant d'Imgur/4Chan le soit
+
+Il vous suffit de remplacer les lignes 8 et 9:
+
+```js
+var anywebM = /http[s]*:\/\/.+.\/.+.(webm)/; //Webm
+var anyimage = /http[s]*:\/\/.+.\/.+.(jpg|gif|png)/; //Webm
+```
+
+par
+
+```js
+var anywebM;
+var anyimage;
+```
+
+### Taille du contenu
+
+
+Il vous suffit de modigier la ligne 12:
+
+```js
+var taille = "25%";
+```
+
+et de mettre par exemple
+
+```js
+var taille = "150px";
+```
+
+## Contact
+
+Si vous souhaitez me contacter pour me demander des informations
+ou pour faire un rapport de bug,
+
+Vous pouvez me contacter ici :
+
+[twitter](https://twitter.com/littletherence)
+[mail](mailto:therence.ferron@gmail.com)
---
+++ script.js
@@ -0,0 +1,83 @@
+//variables pour site 'predefinis'
+var imgur = /http[s]*:\/\/i.imgur.com\/.+.(jpg|gif|png)/; //Image Imgur
+var chanpic = /http[s]*:\/\/i.4cdn.org\/.+.(jpg|gif|png)/; //Image 4Chan
+var chanpic2 = /http[s]*:\/\/is2.4chan.org\/.+.(jpg|gif|png)/; //Image 4Chan
+var chanWebm = /http[s]*:\/\/i.4cdn.org\/.+.(webm)/; //Webm 4Chan
+var webMShare = /http[s]*:\/\/webmshare.com\/.+./; //Webm
+//variables pour n'importe quel site
+var anywebM = /http[s]*:\/\/.+.\/.+.(webm)/; //tous les webms
+var anyimage = /http[s]*:\/\/.+.\/.+.(jpg|gif|png)/; //toutes les images
+
+// variable de la taille
+var taille = "25%";
+
+var liens_hypertext = document.getElementsByClassName("xXx");
+
+for (i in liens_hypertext) {
+  if (imgur.test(liens_hypertext[i].innerHTML) || chanpic.test(liens_hypertext[i].innerHTML) || chanpic2.test(liens_hypertext[i].innerHTML) || anyimage.test(liens_hypertext[i].innerHTML)) {
+
+    console.log(liens_hypertext[i])
+    console.log($(liens_hypertext[i]).attr('target'));
+
+    if ($(liens_hypertext[i]).attr('target') != "_blank") {
+
+      var miniature = document.createElement("img");
+      miniature.className = "img-script";
+      miniature.setAttribute("src", liens_hypertext[i].getAttribute("href"));
+      miniature.setAttribute("alt", "L'image à peut-être été supprimé ou déplacé");
+      miniature.setAttribute("width", taille);
+      miniature.setAttribute("height", taille);
+      //ou cette dimension
+      // miniature.setAttribute("height", "500vh");
+      liens_hypertext[i].innerText = "";
+      liens_hypertext[i].appendChild(miniature);
+      $('[alt]').css({
+        color: 'red',
+        textDecoration: 'underline',
+        fontSize: '20px'
+      });
+    }
+
+  }
+
+  if (chanWebm.test(liens_hypertext[i].innerHTML) || anywebM.test(liens_hypertext[i].innerHTML)) {
+    // console.log('webm');
+    var video = document.createElement("video");
+    video.className = "webm-video";
+    video.setAttribute("controls", "true");
+    video.setAttribute("width", taille);
+    video.setAttribute("height", taille);
+    var source = document.createElement("source");
+    video.className = "webm-source";
+    video.setAttribute("src", liens_hypertext[i].getAttribute("href"));
+    video.setAttribute("type", "video/webm")
+    liens_hypertext[i].innerText = "";
+    liens_hypertext[i].appendChild(video);
+    video.appendChild(source);
+  }
+
+  if (webMShare.test(liens_hypertext[i].innerHTML)) {
+    // console.log('webmShare');
+    var prelink = liens_hypertext[i].getAttribute("href").split('/');
+    // console.log(prelink);
+    var prelink_L = prelink.length;
+    // console.log(prelink_L);
+    var link = prelink[prelink_L - 1];
+    // console.log(link);
+    var linkWebm = "https://s1.webmshare.com/" + link + ".webm";
+    // console.log(linkWebm);
+    var video = document.createElement("video");
+    video.className = "webm-video";
+    video.setAttribute("controls", "true");
+    video.setAttribute("width", taille);
+    video.setAttribute("height", taille);
+    var source = document.createElement("source");
+    video.className = "webm-source";
+    video.setAttribute("src", linkWebm);
+    video.setAttribute("type", "video/webm")
+    liens_hypertext[i].innerText = "";
+    liens_hypertext[i].appendChild(video);
+    video.appendChild(source);
+    source.after("Le webm a probablement été supprimé");
+  }
+}
