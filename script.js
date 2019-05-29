60ae23c883ff6f2f7ba16e190a033a4c74cf3a2c
Therence FERRON <45689459+TherenceFrn@users.noreply.github.com>
2019-05-29 11:40:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,84 +1,66 @@
-//variables pour site 'predefinis'
-var imgur = /http[s]*:\/\/i.imgur.com\/.+.(jpg|gif|png)/; //Image Imgur
-var chanpic = /http[s]*:\/\/i.4cdn.org\/.+.(jpg|gif|png)/; //Image 4Chan
-var chanpic2 = /http[s]*:\/\/is2.4chan.org\/.+.(jpg|gif|png)/; //Image 4Chan
-var chanWebm = /http[s]*:\/\/i.4cdn.org\/.+.(webm)/; //Webm 4Chan
-var webMShare = /http[s]*:\/\/webmshare.com\/.+./; //Webm
-//variables pour n'importe quel site
-var anywebM = /http[s]*:\/\/.+.\/.+.(webm)/; //tous les webms
-var anyimage = /http[s]*:\/\/.+.\/.+.(jpg|gif|png)/; //toutes les images
+// $(document).ready(function(){
+//
+// var svgNumber = 1;
+//
+//   for (var i = 0; i < svgNumber; i++) {
+//
+//     var j = i+1;
+//     $('.AllSvg').append('<img src="'+j+'.svg">');
+//   }
+//
+// });
 
-// variable de la taille
-//var taille = "25%";
-var taille = "100%";
 
-var liens_hypertext = document.getElementsByClassName("xXx");
+var contenu = [
+  ['Illustrator', 4],
+  ['Photoshop', 1],
+  ['Lightroom', 1],
+  ['Animate', 1],
+]
 
-for (i in liens_hypertext) {
-  if (imgur.test(liens_hypertext[i].innerHTML) || chanpic.test(liens_hypertext[i].innerHTML) || chanpic2.test(liens_hypertext[i].innerHTML) || anyimage.test(liens_hypertext[i].innerHTML)) {
+// console.log(contenu.length);
+for (var i = 0; i < contenu.length; i++) {
 
-    console.log(liens_hypertext[i])
-    console.log($(liens_hypertext[i]).attr('target'));
+  // console.log(contenu[i][0]);
 
-    if ($(liens_hypertext[i]).attr('target') != "_blank") {
+  var logiciel = contenu[i][0];
+  var contenuLogiciel = contenu[i][1];
+  var contenuPage = '<div class="container"><div class="row">';
+  var compteCol = 0;
+  var extension = '';
+  // console.log(logiciel+' : '+contenuLogiciel);
 
-      var miniature = document.createElement("img");
-      miniature.className = "img-script";
-      miniature.setAttribute("src", liens_hypertext[i].getAttribute("href"));
-      miniature.setAttribute("alt", "L'image à peut-être été supprimé ou déplacé");
-      miniature.setAttribute("width", taille);
-      miniature.setAttribute("height", taille);
-      //ou cette dimension
-      // miniature.setAttribute("height", "500vh");
-      liens_hypertext[i].innerText = "";
-      liens_hypertext[i].appendChild(miniature);
-      $('[alt]').css({
-        color: 'red',
-        textDecoration: 'underline',
-        fontSize: '20px'
-      });
+  for (var j = 0; j < contenuLogiciel; j++) {
+
+    if(compteCol == 3){
+      contenuPage = contenuPage +  '</div><br><div class="row">';
+      compteCol = 0;
     }
 
+    switch (logiciel) {
+      case 'Illustrator':
+        var extension = 'svg';
+      break;
+      case 'Photoshop':
+        var extension = 'png';
+      break;
+      case 'Lightroom':
+        var extension = 'jpg';
+      break;
+      case 'Animate':
+        var extension = 'gif';
+      break;
+    }
+
+    var y = j+1;
+
+    console.log(y+'.'+extension);
+
+    contenuPage = contenuPage + '<div class="col-sm-4"><div class="card" style="width:100%"><img src="'+y+'.'+extension+'"><div class="card-body"><h4 class="card-title">Imgur</h4><p class="card-text">Exemple</p><a target="blank" href="https://therencefrn.github.io/ArtStuff/'+y+'.'+extension+'" class="text-primary">https://therencefrn.github.io/ArtStuff/'+y+'.'+extension+'</a></div></div></div>';
+    compteCol++;
   }
 
-  if (chanWebm.test(liens_hypertext[i].innerHTML) || anywebM.test(liens_hypertext[i].innerHTML)) {
-    // console.log('webm');
-    var video = document.createElement("video");
-    video.className = "webm-video";
-    video.setAttribute("controls", "true");
-    video.setAttribute("width", taille);
-    video.setAttribute("height", taille);
-    var source = document.createElement("source");
-    video.className = "webm-source";
-    video.setAttribute("src", liens_hypertext[i].getAttribute("href"));
-    video.setAttribute("type", "video/webm")
-    liens_hypertext[i].innerText = "";
-    liens_hypertext[i].appendChild(video);
-    video.appendChild(source);
-  }
+  var divCible = "#"+logiciel;
+  $(divCible).after(contenuPage);
 
-  if (webMShare.test(liens_hypertext[i].innerHTML)) {
-    // console.log('webmShare');
-    var prelink = liens_hypertext[i].getAttribute("href").split('/');
-    // console.log(prelink);
-    var prelink_L = prelink.length;
-    // console.log(prelink_L);
-    var link = prelink[prelink_L - 1];
-    // console.log(link);
-    var linkWebm = "https://s1.webmshare.com/" + link + ".webm";
-    // console.log(linkWebm);
-    var video = document.createElement("video");
-    video.className = "webm-video";
-    video.setAttribute("controls", "true");
-    video.setAttribute("width", taille);
-    video.setAttribute("height", taille);
-    var source = document.createElement("source");
-    video.className = "webm-source";
-    video.setAttribute("src", linkWebm);
-    video.setAttribute("type", "video/webm")
-    liens_hypertext[i].innerText = "";
-    liens_hypertext[i].appendChild(video);
-    video.appendChild(source);
-    source.after("Le webm a probablement été supprimé");
-  }
 }
